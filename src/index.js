949394b0c05ae14933c86f70400ed19f2883f02f
vadsolid <maguscode@protonmail.com>
2022-02-16 22:13:55 -0500

    init: start and finish magic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(num) {
+   if (num === 0) return "zero";
+   const onceRange = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+   const tensRange = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+   const dozensRange = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+   const hundredsRange = parseInt(num/100);
+   const digitOfTeens = num % 100;
+   const digitOfUnits = num % 10;
+   if (digitOfTeens <= 19 && digitOfTeens >= 10) {
+       let result = `${onceRange[hundredsRange]}${!(hundredsRange) ? "" : " hundred"} ${tensRange[digitOfUnits]}`
+       return result.replace(/\s+/g, " ").trim();
+    }
+   const digitOfDozens = (num % 100 - digitOfUnits) / 10;
+   let result = `${onceRange[hundredsRange]}${!(hundredsRange) ? "" : " hundred"} ${dozensRange[digitOfDozens]} ${onceRange[digitOfUnits]}`;
+   return result.replace(/\s+/g, " ").trim();
 }
